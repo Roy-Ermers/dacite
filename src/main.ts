ac6bb6dd20054925c1e8373225b91b21f3d5b714
Roy Ermers <royermersgeffen@gmail.com>
2025-02-19 23:49:34 +0100

    Fix build by not doing top level await

--- src/main.ts
+++ src/main.ts
@@ -22,53 +22,57 @@ import Vector2 from "./utils/Vector2";
 
 
 const game = new Game();
-await game.init();
+async function init() {
+    await game.init();
 
-await Resources.load();
-game.addSystem(
-    new RenderSystem,
-    new RigidbodySystem,
-    new PlayerControllerSystem,
-    new CameraSystem,
-    new ColliderSystem,
-    new CollisionDebuggerSystem
-);
+    await Resources.load();
+    game.addSystem(
+        new RenderSystem,
+        new RigidbodySystem,
+        new PlayerControllerSystem,
+        new CameraSystem,
+        new ColliderSystem,
+        new CollisionDebuggerSystem
+    );
 
-TileManager.register(new TexturedTile('wall').setSolid());
-TileManager.register(new TexturedTile('floor'));
-TileManager.register(new TexturedTile('floor_embers'));
-TileManager.register(new TexturedTile('floor_moss'));
+    TileManager.register(new TexturedTile('wall').setSolid());
+    TileManager.register(new TexturedTile('floor'));
+    TileManager.register(new TexturedTile('floor_embers'));
+    TileManager.register(new TexturedTile('floor_moss'));
 
-const tileMap = new TileMap(64, 64);
+    const tileMap = new TileMap(64, 64);
 
-for (let x = 0; x < tileMap.width; x++) {
-    for (let y = 0; y < tileMap.height; y++) {
-        if (x === 0 || y === 0 || x === tileMap.width - 1 || y === tileMap.height - 1) {
-            tileMap.setTile(x, y, "wall");
-            continue;
+    for (let x = 0; x < tileMap.width; x++) {
+        for (let y = 0; y < tileMap.height; y++) {
+            if (x === 0 || y === 0 || x === tileMap.width - 1 || y === tileMap.height - 1) {
+                tileMap.setTile(x, y, "wall");
+                continue;
+            }
+
+            if (Math.random() < 0.1)
+                tileMap.setTile(x, y, 'floor_embers');
+            else if (Math.random() < 0.2)
+                tileMap.setTile(x, y, 'floor_moss');
+            else
+                tileMap.setTile(x, y, 'floor');
         }
-
-        if (Math.random() < 0.1)
-            tileMap.setTile(x, y, 'floor_embers');
-        else if (Math.random() < 0.2)
-            tileMap.setTile(x, y, 'floor_moss');
-        else
-            tileMap.setTile(x, y, 'floor');
     }
+
+    const renderer = new TileMapRenderer(tileMap, Resources.TileSpritesheet);
+    const tileMapEntity = new Entity()
+        .add(renderer)
+        .add(new Renderable(renderer.container))
+        .add(new Transform());
+
+    const tile = new Entity()
+        .add(new Renderable(Sprite.from(Resources.TileSpritesheet.textures['wall'])))
+        .add(new Transform(new Vector2(-32, -32)))
+        .add(new Rigidbody({ type: "dynamic", mass: 10000 }))
+        .add(new BoxCollider(64, 16), Collider);
+
+    game.ecs.addEntity(tile);
+    game.ecs.addEntity(tileMapEntity);
+    game.ecs.addEntity(createPlayer());
 }
 
-const renderer = new TileMapRenderer(tileMap, Resources.TileSpritesheet);
-const tileMapEntity = new Entity()
-    .add(renderer)
-    .add(new Renderable(renderer.container))
-    .add(new Transform());
-
-const tile = new Entity()
-    .add(new Renderable(Sprite.from(Resources.TileSpritesheet.textures['wall'])))
-    .add(new Transform(new Vector2(-32, -32)))
-    .add(new Rigidbody({ type: "dynamic", mass: 10000 }))
-    .add(new BoxCollider(64, 16), Collider);
-
-game.ecs.addEntity(tile);
-game.ecs.addEntity(tileMapEntity);
-game.ecs.addEntity(createPlayer());
+init();
